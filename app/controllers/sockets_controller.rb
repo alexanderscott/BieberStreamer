eb16c134b79db99ddb393f1eb8434f59d73e645f
Alex Ehrnschwender <alex.ehrnschwender@dena.com>
2014-03-08 01:44:07 -0800

    fixes threading using rackup, adds GA code

--- app/controllers/sockets_controller.rb
+++ app/controllers/sockets_controller.rb
@@ -1,36 +1,24 @@
-require 'twitter_streaming'
-require 'redis'
-
+require 'json'
 class SocketsController < ApplicationController
   include ActionController::Live
-
-  def initialize
-  end
-
   def stream
-    
     response.headers['Content-Type'] = 'text/event-stream'
-
-    sse = TwitterStreaming::SocketClient.new response.stream
-
-    #Thread.new do 
-      redis_sub = Redis.new
-      redis_sub.subscribe("streams:trend:bieber") do |on|
-        on.message do |channel, tweet_json|
-          #puts "Received message from #{channel.to_s} :: #{tweet_json}"
-          sse.write( tweet_json, event: 'tweet')
-        end
+    redis_sub = Redis.new
+    redis_sub.subscribe("streams:trend:bieber") do |on|
+      on.message do |channel, tweet_json|
+        ##puts "Received message from #{channel.to_s} :: #{tweet_json}"
+        response.stream.write(sse(tweet_json, {event: 'tweet'}))
       end
-      #render nothing: true
-    #end
-
-    render nothing: true
-
-    rescue IOError
-      puts "IO ERROR"
-    ensure
-      response.stream.close unless !response.stream
-      sse.close unless !sse
+    end
+  rescue IOError
+  # Client Disconnected
+  ensure
+    response.stream.close
   end
 
+  private
+
+  def sse(object, options = {})
+    (options.map{|k,v| "#{k}: #{v}" } << "data: #{object}").join("\n") + "\n\n"
+  end
 end
--- config.ru
+++ config.ru
@@ -1,4 +1,22 @@
 # This file is used by Rack-based servers to start the application.
 
+require 'yaml'
+require './lib/twitter_streaming'
+
 require ::File.expand_path('../config/environment',  __FILE__)
+
+env_file = File.join('./config', 'environment.yml')
+YAML.load(File.open(env_file))['development'].each do |key, value|
+  ENV[key.to_s] = value
+end if File.exists?(env_file)
+
+Thread.new do
+  TwitterStreaming::StreamWorker.new({
+    :consumer_key => ENV['TWITTER_CONSUMER_KEY'],
+    :consumer_secret => ENV['TWITTER_CONSUMER_SECRET'],
+    :access_token => ENV['TWITTER_ACCESS_TOKEN'],
+    :access_token_secret => ENV['TWITTER_ACCESS_TOKEN_SECRET']
+  })
+end
+
 run Rails.application
